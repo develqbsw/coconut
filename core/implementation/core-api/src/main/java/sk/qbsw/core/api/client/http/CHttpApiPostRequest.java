77b31aaf4fa384aa5b1f40c9b71229e8326b7fa4
Dalibor Rak <rak@qbsw.sk>
2013-01-25 12:28:21 +0000

    CNUT-21

--- core/implementation/core-api/src/main/java/sk/qbsw/core/api/client/http/CHttpApiPostRequest.java
+++ core/implementation/core-api/src/main/java/sk/qbsw/core/api/client/http/CHttpApiPostRequest.java
@@ -10,6 +10,8 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
 
 import sk.qbsw.core.api.exception.CApiHttpException;
 
@@ -40,9 +42,12 @@ public class CHttpApiPostRequest extends AHttpApiRequest implements IHttpApiRequ
 		try
 		{
 			DefaultHttpClient httpClient = new DefaultHttpClient();
+			HttpParams params = httpClient.getParams();
+			HttpConnectionParams.setConnectionTimeout(params, 10000);
+			HttpConnectionParams.setSoTimeout(params, 10000);
+			
 			HttpPost postRequest = new HttpPost(url);
 			postRequest.addHeader("accept", contentType.getMimeType());
-
 			if (entityInJSon != null)
 			{
 				StringEntity input = new StringEntity(entityInJSon);
