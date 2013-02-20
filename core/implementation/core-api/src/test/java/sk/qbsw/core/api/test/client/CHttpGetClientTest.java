98a161c5cffce9763a1df468589a91c32d66e616
Dalibor Rak <rak@qbsw.sk>
2013-02-20 14:26:56 +0000

    CNUT-27

--- core/implementation/core-api/src/test/java/sk/qbsw/core/api/test/client/CHttpGetClientTest.java
+++ core/implementation/core-api/src/test/java/sk/qbsw/core/api/test/client/CHttpGetClientTest.java
@@ -5,15 +5,24 @@ import org.junit.Test;
 import sk.qbsw.core.api.client.CApiClient;
 import sk.qbsw.core.api.client.http.CHttpApiPostRequest;
 
-public class CHttpGetClientTest
+/**
+ * Creates test call to mscan.qsbw.local (Be sure, that the link is available
+ * without proxy settings)
+ * 
+ * @author Dalibor Rak
+ * @version 1.3.0
+ * @since 1.3.0
+ * 
+ */
+public class CHttpGetClientTest 
 {
 	@Test
-	public void test ()
+	public void test() 
 	{
 		CApiClient<CCallRequestModel, CCallResponseModel> client = new CApiClient<CCallRequestModel, CCallResponseModel>();
 		CHttpApiPostRequest post = new CHttpApiPostRequest();
 		post.setRepeatCount(2);
-		
-		client.makeCall(post, "http://www.myxtension.com", new CCallRequestModel());
+
+		client.makeCall(post, "http://mscan.qbsw.local/admin/index.html", new CCallRequestModel());
 	}
 }
