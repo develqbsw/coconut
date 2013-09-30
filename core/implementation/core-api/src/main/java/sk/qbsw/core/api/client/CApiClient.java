5797771b46731b7aab9603dd6ccab08e8181db42
Dagmar Havelkova <havelkova@qbsw.sk>
2013-09-30 08:33:08 +0000

    CNUT-57

--- core/implementation/core-api/src/main/java/sk/qbsw/core/api/client/CApiClient.java
+++ core/implementation/core-api/src/main/java/sk/qbsw/core/api/client/CApiClient.java
@@ -2,9 +2,11 @@ package sk.qbsw.core.api.client;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.net.URLEncoder;
 import java.util.Date;
 
 import org.apache.http.entity.ContentType;
+import org.apache.log4j.Logger;
 
 import sk.qbsw.core.api.client.gson.serializer.CDateJSonSerializer;
 import sk.qbsw.core.api.client.http.IHttpApiRequest;
@@ -14,35 +16,61 @@ import com.google.gson.GsonBuilder;
 
 /**
  * API Client - combination of HTTP request and GSon parser.
- *
- * @param <I> input class
- * @param <O> output class
+ * 
+ * @param <I>
+ *            input class
+ * @param <O>
+ *            output class
  * @author Dalibor Rak
  * @version 1.3.0
  * @since 1.2.0
  */
 public class CApiClient<I, O>
 {
+	protected static final Logger LOGGER = Logger.getLogger(CApiClient.class);
+
 	/** Gson builder to create gson. */
 	private GsonBuilder builder;
+
+	/** The encode api value. */
+	private boolean encodeApiValue;
 
 	/**
 	 * Instantiates a new api client.
 	 */
 	public CApiClient ()
 	{
-		//create builder
+		// create builder
 		this.builder = new GsonBuilder();
-		//initialize builder and register default adapters
+		// initialize builder and register default adapters
+		this.builder = prepareGsonBuilder(builder);
+	}
+
+	/**
+	 * Instantiates a new c api client.
+	 * 
+	 * @param encodeApiValue
+	 *            the encode api value
+	 */
+	public CApiClient (boolean encodeApiValue)
+	{
+		this.encodeApiValue = encodeApiValue;
+
+		// create builder
+		this.builder = new GsonBuilder();
+		// initialize builder and register default adapters
 		this.builder = prepareGsonBuilder(builder);
 	}
 
 	/**
 	 * Make call to API.
-	 *
-	 * @param request the request
-	 * @param url the url
-	 * @param input the input
+	 * 
+	 * @param request
+	 *            the request
+	 * @param url
+	 *            the url
+	 * @param input
+	 *            the input
 	 * @return the string
 	 */
 	public String makeCall (IHttpApiRequest request, String url, I input) throws IOException
@@ -52,11 +80,15 @@ public class CApiClient<I, O>
 
 	/**
 	 * Makes call to API.
-	 *
-	 * @param request request to use
-	 * @param url URL to API
-	 * @param input input parameter
-	 * @param returnClass Class to return
+	 * 
+	 * @param request
+	 *            request to use
+	 * @param url
+	 *            URL to API
+	 * @param input
+	 *            input parameter
+	 * @param returnClass
+	 *            Class to return
 	 * @return Returned object (instance of returnClass)
 	 */
 	public O makeCall (IHttpApiRequest request, String url, I input, Class<O> returnClass) throws IOException
@@ -66,26 +98,35 @@ public class CApiClient<I, O>
 
 	/**
 	 * Make call with specific content.
-	 *
-	 * @param request the request
-	 * @param url the url
-	 * @param input the input
-	 * @param returnClass the return class
-	 * @param type the type
+	 * 
+	 * @param request
+	 *            the request
+	 * @param url
+	 *            the url
+	 * @param input
+	 *            the input
+	 * @param returnClass
+	 *            the return class
+	 * @param type
+	 *            the type
 	 * @return the o
 	 */
 	public O makeCall (IHttpApiRequest request, String url, I input, Class<O> returnClass, ContentType type) throws IOException
 	{
 		return makeCall(request, url, input, (Type) returnClass, type);
 	}
-	
+
 	/**
 	 * Makes call with specific content.
-	 *
-	 * @param request request to use
-	 * @param url URL to API
-	 * @param input input parameter
-	 * @param contentType the content type
+	 * 
+	 * @param request
+	 *            request to use
+	 * @param url
+	 *            URL to API
+	 * @param input
+	 *            input parameter
+	 * @param contentType
+	 *            the content type
 	 * @return Returned object (instance of returnClass)
 	 */
 	public String makeCall (IHttpApiRequest request, String url, I input, ContentType contentType) throws IOException
@@ -96,6 +137,11 @@ public class CApiClient<I, O>
 		// prepare request
 		String requestJson = gson.toJson(input);
 
+		if (encodeApiValue)
+		{
+			requestJson = URLEncoder.encode(requestJson, "UTF8");
+		}
+
 		// process request
 		String response = request.makeCall(url, contentType, requestJson);
 
@@ -104,11 +150,15 @@ public class CApiClient<I, O>
 
 	/**
 	 * Make call to API.
-	 *
-	 * @param request the request
-	 * @param url the url
-	 * @param input the input
-	 * @param returnType the return type
+	 * 
+	 * @param request
+	 *            the request
+	 * @param url
+	 *            the url
+	 * @param input
+	 *            the input
+	 * @param returnType
+	 *            the return type
 	 * @return the o
 	 */
 	public O makeCall (IHttpApiRequest request, String url, I input, Type returnType) throws IOException
@@ -118,12 +168,17 @@ public class CApiClient<I, O>
 
 	/**
 	 * Makes call with specific content.
-	 *
-	 * @param request request to use
-	 * @param url URL to API
-	 * @param input input parameter
-	 * @param returnType Type to return
-	 * @param contentType the content type
+	 * 
+	 * @param request
+	 *            request to use
+	 * @param url
+	 *            URL to API
+	 * @param input
+	 *            input parameter
+	 * @param returnType
+	 *            Type to return
+	 * @param contentType
+	 *            the content type
 	 * @return Returned object (instance of returnClass)
 	 */
 	@SuppressWarnings ("unchecked")
@@ -140,11 +195,12 @@ public class CApiClient<I, O>
 		return responseObject;
 	}
 
-
 	/**
-	 * Initializes Gson Builder - register standard date serializer/deserializer.
-	 *
-	 * @param builder builder to initialize
+	 * Initializes Gson Builder - register standard date
+	 * serializer/deserializer.
+	 * 
+	 * @param builder
+	 *            builder to initialize
 	 * @return the gson builder
 	 */
 	protected GsonBuilder prepareGsonBuilder (GsonBuilder builder)
@@ -152,14 +208,14 @@ public class CApiClient<I, O>
 		return this.builder.registerTypeAdapter(Date.class, new CDateJSonSerializer());
 	}
 
-
-
-
 	/**
-	 * register adapter to request on server(If class implements JsonSerializer) and for response from server(If class implements JsonDeserializer).
-	 *
-	 * @param type for which is adapter registered
-	 * @param typeAdapter adapter to register
+	 * register adapter to request on server(If class implements JsonSerializer)
+	 * and for response from server(If class implements JsonDeserializer).
+	 * 
+	 * @param type
+	 *            for which is adapter registered
+	 * @param typeAdapter
+	 *            adapter to register
 	 */
 	public void registerTypeAdapter (Type type, Object typeAdapter)
 	{
