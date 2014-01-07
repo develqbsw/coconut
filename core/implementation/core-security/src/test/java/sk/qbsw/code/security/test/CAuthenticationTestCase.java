a53987f9e64808729eafbf3ad604245fb9cf7520
Tomas Lauro <lauro@qbsw.sk>
2014-01-07 17:21:53 +0000

    CNUT-62 - pridana cast unit testov

--- core/implementation/core-security/src/test/java/sk/qbsw/code/security/test/CAuthenticationTestCase.java
+++ core/implementation/core-security/src/test/java/sk/qbsw/code/security/test/CAuthenticationTestCase.java
@@ -1,28 +1,42 @@
 package sk.qbsw.code.security.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
+import java.util.Iterator;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
+import sk.qbsw.code.security.test.util.CDatabaseDataGenerator;
+import sk.qbsw.core.security.exception.CSecurityException;
+import sk.qbsw.core.security.model.domain.CGroup;
+import sk.qbsw.core.security.model.domain.CRole;
+import sk.qbsw.core.security.model.domain.CUser;
 import sk.qbsw.core.security.service.IAuthenticationService;
 
 /**
- * Checks Authentication service
- * 
+ * Checks Authentication service.
+ *
+ * @autor Tomas Lauro
  * @version 1.6.0
  * @since 1.6.0
  */
 @RunWith (SpringJUnit4ClassRunner.class)
 @ContextConfiguration (locations = {"classpath:/spring/test-context.xml"})
-@TransactionConfiguration (transactionManager = "transactionManager")
+@TransactionConfiguration (transactionManager = "transactionManager", defaultRollback = true)
 public class CAuthenticationTestCase
 {
+	/** The database data generator. */
+	@Autowired
+	private CDatabaseDataGenerator databaseDataGenerator;
 
 	/** The authentication service. */
 	@Autowired
@@ -35,7 +49,90 @@ public class CAuthenticationTestCase
 	@Test
 	public void testInitialization ()
 	{
-		assertNotNull("Couldnt find Authentication service.", authenticationService);
+		assertNotNull("Couldnt find Authentication service", authenticationService);
 	}
 
+	/**
+	 * Login user.
+	 *
+	 * @throws CSecurityException the c security exception
+	 */
+	@Test
+	@Transactional
+	@Rollback (true)
+	public void testLogin () throws CSecurityException
+	{
+		databaseDataGenerator.generateDataForAuthenticationTests();
+
+		testLoginWithDefaultUnit();
+		testLoginWithoutDefaultUnit();
+		testLoginWithDefaultUnitAndRole();
+	}
+
+	/**
+	 * Test login with default unit.
+	 *
+	 * @throws CSecurityException the security exception
+	 */
+	private void testLoginWithDefaultUnit () throws CSecurityException
+	{
+		CUser user = authenticationService.login("user_with_default_unit", "user_with_default_unit");
+		assertNotNull("Authentication with login and password failed: user is null", user);
+		assertNotNull("Authentication with login and password failed: user groups is null", user.getGroups());
+		Assert.assertEquals("Authentication with login and password failed: number of user groups is not 3", user.getGroups().size(), 3);
+	}
+
+	/**
+	 * Test login without default unit.
+	 *
+	 * @throws CSecurityException the security exception
+	 */
+	private void testLoginWithoutDefaultUnit () throws CSecurityException
+	{
+		CUser user = authenticationService.login("user_without_default_unit", "user_without_default_unit");
+		assertNotNull("Authentication with login and password failed: user is null", user);
+		assertNotNull("Authentication with login and password failed: user groups is null", user.getGroups());
+		Assert.assertEquals("Authentication with login and password failed: number of user groups is not 2", user.getGroups().size(), 2);
+	}
+
+	/**
+	 * Test login with default unit and role.
+	 *
+	 * @throws CSecurityException the security exception
+	 */
+	private void testLoginWithDefaultUnitAndRole () throws CSecurityException
+	{
+		CRole inputRole = new CRole("role_1");
+		CUser user = authenticationService.login("user_with_default_unit", "user_with_default_unit", inputRole);
+		assertNotNull("Authentication with login, password and role failed: user is null", user);
+		assertNotNull("Authentication with login, password and role failed: user groups is null", user.getGroups());
+		Assert.assertEquals("Authentication with login, password and role failed: number of user groups is not 2", user.getGroups().size(), 3);
+
+		CRole outputRole = null;
+		Iterator<CGroup> groupIterator = user.getGroups().iterator();
+		while (groupIterator.hasNext())
+		{
+			CGroup group = (CGroup) groupIterator.next();
+			assertNotNull("Authentication with login, password and role failed: user roles in group " + group.getCode() + " is null", group.getRoles());
+
+			Iterator<CRole> roleIterator = group.getRoles().iterator();
+			while (roleIterator.hasNext())
+			{
+				CRole tempOutputRole = (CRole) roleIterator.next();
+				if (tempOutputRole.getCode().equals(inputRole.getCode()))
+				{
+					outputRole = tempOutputRole;
+					break;
+				}
+
+			}
+
+			if (outputRole != null)
+			{
+				break;
+			}
+
+		}
+		assertNotNull("Authentication with login, password and role failed: user has not a requested role " + inputRole.getCode(), outputRole);
+	}
 }
---
+++ core/implementation/core-security/src/test/java/sk/qbsw/code/security/test/util/CDatabaseDataGenerator.java
@@ -0,0 +1,243 @@
+package sk.qbsw.code.security.test.util;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import sk.qbsw.core.security.dao.IGroupDao;
+import sk.qbsw.core.security.dao.IOrganizationDao;
+import sk.qbsw.core.security.dao.IRoleDao;
+import sk.qbsw.core.security.dao.IUnitDao;
+import sk.qbsw.core.security.dao.IUserDao;
+import sk.qbsw.core.security.model.domain.CGroup;
+import sk.qbsw.core.security.model.domain.COrganization;
+import sk.qbsw.core.security.model.domain.CRole;
+import sk.qbsw.core.security.model.domain.CUnit;
+import sk.qbsw.core.security.model.domain.CUser;
+
+/**
+ * Generate data in DB for tests.
+ *
+ * @autor Tomas Lauro
+ * @version 1.6.0
+ * @since 1.6.0
+ */
+@Component (value = "databaseDataGenerator")
+public class CDatabaseDataGenerator
+{
+	/** The role dao. */
+	@Autowired
+	private IRoleDao roleDao;
+
+	/** The user dao. */
+	@Autowired
+	private IUserDao userDao;
+
+	/** The org dao. */
+	@Autowired
+	private IOrganizationDao orgDao;
+
+	/** The unit dao. */
+	@Autowired
+	private IUnitDao unitDao;
+
+	/** The group dao. */
+	@Autowired
+	private IGroupDao groupDao;
+
+	/**
+	 * Generate data for authentication tests.
+	 */
+	public void generateDataForAuthenticationTests ()
+	{
+		/** Create data. */
+		//organization
+		COrganization organization = new COrganization();
+		organization.setCode("organization");
+		organization.setName("organization");
+		organization.setEmail("organization@qbsw.sk");
+		organization.setPhone("000000000");
+		organization.setFlagEnabled(true);
+
+		//roles
+		CRole firstRole = new CRole();
+		firstRole.setCode("role_1");
+
+		CRole secondRole = new CRole();
+		secondRole.setCode("role_2");
+
+		//groups
+		CGroup firstGroupInUnit = new CGroup();
+		firstGroupInUnit.setCode("group_in_unit_1");
+		firstGroupInUnit.setCategory("category_1");
+
+		CGroup secondGroupInUnit = new CGroup();
+		secondGroupInUnit.setCode("group_in_unit_2");
+		secondGroupInUnit.setCategory("category_1");
+
+		CGroup thirdGroupInUnit = new CGroup();
+		thirdGroupInUnit.setCode("group_in_unit_3");
+		thirdGroupInUnit.setCategory(null);
+
+		CGroup firstGroupNotInUnit = new CGroup();
+		firstGroupNotInUnit.setCode("group_not_in_unit_1");
+		firstGroupNotInUnit.setCategory(null);
+
+		CGroup secondGroupNotInUnit = new CGroup();
+		secondGroupNotInUnit.setCode("group_not_in_unit_2");
+		secondGroupNotInUnit.setCategory("category_2");
+
+		//units
+		CUnit defaultUnit = new CUnit();
+		defaultUnit.setName("default_unit");
+
+		CUnit firstUnit = new CUnit();
+		firstUnit.setName("unit_1");
+
+		CUnit secondUnit = new CUnit();
+		secondUnit.setName("unit_2");
+
+		//users
+		CUser userWithDefaultUnit = new CUser();
+		userWithDefaultUnit.setLogin("user_with_default_unit");
+		userWithDefaultUnit.setPassword("user_with_default_unit");
+		userWithDefaultUnit.setName("user_with_default_unit");
+		userWithDefaultUnit.setSurname("user_with_default_unit");
+		userWithDefaultUnit.setEmail("user_with_default_unit@qbsw.sk");
+		userWithDefaultUnit.setFlagEnabled(true);
+
+		CUser userWithoutDefaultUnit = new CUser();
+		userWithoutDefaultUnit.setLogin("user_without_default_unit");
+		userWithoutDefaultUnit.setPassword("user_without_default_unit");
+		userWithoutDefaultUnit.setName("user_without_default_unit");
+		userWithoutDefaultUnit.setSurname("user_without_default_unit");
+		userWithoutDefaultUnit.setEmail("user_without_default_uni@qbsw.sk");
+		userWithoutDefaultUnit.setFlagEnabled(true);
+
+		/** Create connections. */
+		//for units
+		Set<CGroup> groupsForDefaultUnit = new HashSet<CGroup>();
+		groupsForDefaultUnit.add(firstGroupInUnit);
+		groupsForDefaultUnit.add(secondGroupInUnit);
+
+		Set<CGroup> groupsForFirstUnit = new HashSet<CGroup>();
+		groupsForFirstUnit.add(secondGroupInUnit);
+		groupsForFirstUnit.add(thirdGroupInUnit);
+
+		Set<CGroup> groupsForSecondUnit = new HashSet<CGroup>();
+		groupsForSecondUnit.add(firstGroupInUnit);
+		groupsForSecondUnit.add(thirdGroupInUnit);
+
+		defaultUnit.setOrganization(organization);
+		firstUnit.setOrganization(organization);
+		secondUnit.setOrganization(organization);
+
+		defaultUnit.setGroups(groupsForDefaultUnit);
+		firstUnit.setGroups(groupsForFirstUnit);
+		secondUnit.setGroups(groupsForSecondUnit);
+
+		//for groups
+		Set<CUnit> unitsForFirstGroupInUnit = new HashSet<CUnit>();
+		unitsForFirstGroupInUnit.add(defaultUnit);
+		unitsForFirstGroupInUnit.add(secondUnit);
+
+		Set<CUnit> unitsForSecondGroupInUnit = new HashSet<CUnit>();
+		unitsForSecondGroupInUnit.add(defaultUnit);
+		unitsForSecondGroupInUnit.add(firstUnit);
+
+		Set<CUnit> unitsForThirdGroupInUnit = new HashSet<CUnit>();
+		unitsForThirdGroupInUnit.add(firstUnit);
+		unitsForThirdGroupInUnit.add(secondUnit);
+
+		Set<CRole> rolesForFirstGroupInUnit = new HashSet<CRole>();
+		rolesForFirstGroupInUnit.add(firstRole);
+
+		Set<CRole> rolesForSecondGroupInUnit = new HashSet<CRole>();
+		rolesForSecondGroupInUnit.add(firstRole);
+
+		Set<CRole> rolesForThirdGroupInUnit = new HashSet<CRole>();
+		rolesForThirdGroupInUnit.add(secondRole);
+
+		Set<CRole> rolesForFirstGroupNotInUnit = new HashSet<CRole>();
+		rolesForFirstGroupNotInUnit.add(firstRole);
+
+		Set<CRole> rolesForSecondGroupNotInUnit = new HashSet<CRole>();
+		rolesForSecondGroupNotInUnit.add(firstRole);
+
+		Set<CRole> rolesForThirdGroupNotInUnit = new HashSet<CRole>();
+		rolesForThirdGroupNotInUnit.add(secondRole);
+
+		Set<CUser> usersForGroupsInUnit = new HashSet<CUser>();
+		usersForGroupsInUnit.add(userWithDefaultUnit);
+
+		Set<CUser> usersForGroupsNotInUnit = new HashSet<CUser>();
+		usersForGroupsNotInUnit.add(userWithoutDefaultUnit);
+
+		firstGroupInUnit.setUnits(unitsForFirstGroupInUnit);
+		secondGroupInUnit.setUnits(unitsForSecondGroupInUnit);
+		thirdGroupInUnit.setUnits(unitsForThirdGroupInUnit);
+
+		firstGroupInUnit.setRoles(rolesForFirstGroupInUnit);
+		secondGroupInUnit.setRoles(rolesForSecondGroupInUnit);
+		thirdGroupInUnit.setRoles(rolesForThirdGroupInUnit);
+
+		firstGroupNotInUnit.setRoles(rolesForFirstGroupNotInUnit);
+		secondGroupNotInUnit.setRoles(rolesForSecondGroupNotInUnit);
+
+		firstGroupInUnit.setUsers(usersForGroupsInUnit);
+		secondGroupInUnit.setUsers(usersForGroupsInUnit);
+		thirdGroupInUnit.setUsers(usersForGroupsInUnit);
+
+		firstGroupNotInUnit.setUsers(usersForGroupsNotInUnit);
+		secondGroupNotInUnit.setUsers(usersForGroupsNotInUnit);
+
+		//for roles
+		Set<CGroup> groupsForFirstRole = new HashSet<CGroup>();
+		groupsForFirstRole.add(firstGroupInUnit);
+		groupsForFirstRole.add(secondGroupInUnit);
+		groupsForFirstRole.add(secondGroupNotInUnit);
+		groupsForFirstRole.add(secondGroupNotInUnit);
+
+		Set<CGroup> groupsForSecondRole = new HashSet<CGroup>();
+		groupsForSecondRole.add(thirdGroupInUnit);
+
+		firstRole.setGroups(groupsForFirstRole);
+		secondRole.setGroups(groupsForSecondRole);
+
+		//for user
+		Set<CGroup> groupsForUserWithDefaultUnit = new HashSet<CGroup>();
+		groupsForUserWithDefaultUnit.add(firstGroupInUnit);
+		groupsForUserWithDefaultUnit.add(secondGroupInUnit);
+		groupsForUserWithDefaultUnit.add(thirdGroupInUnit);
+
+		Set<CGroup> groupsForUserWithoutDefaultUnit = new HashSet<CGroup>();
+		groupsForUserWithoutDefaultUnit.add(firstGroupNotInUnit);
+		groupsForUserWithoutDefaultUnit.add(secondGroupNotInUnit);
+
+		userWithDefaultUnit.setOrganization(organization);
+		userWithoutDefaultUnit.setOrganization(organization);
+
+		userWithDefaultUnit.setDefaultUnit(defaultUnit);
+		userWithoutDefaultUnit.setDefaultUnit(null);
+
+		userWithDefaultUnit.setGroups(groupsForUserWithDefaultUnit);
+		userWithoutDefaultUnit.setGroups(groupsForUserWithoutDefaultUnit);
+
+		//save data to DB
+		orgDao.save(organization);
+		roleDao.save(firstRole);
+		roleDao.save(secondRole);
+		groupDao.save(firstGroupInUnit);
+		groupDao.save(secondGroupInUnit);
+		groupDao.save(thirdGroupInUnit);
+		groupDao.save(firstGroupNotInUnit);
+		groupDao.save(secondGroupNotInUnit);
+		unitDao.save(defaultUnit);
+		unitDao.save(firstUnit);
+		unitDao.save(secondUnit);
+		userDao.save(userWithDefaultUnit);
+		userDao.save(userWithoutDefaultUnit);
+	}
+}
