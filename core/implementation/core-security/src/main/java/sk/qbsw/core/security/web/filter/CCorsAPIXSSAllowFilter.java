73e195ea47ca2de842fca6922d4fe487f4e6c6d0
Dalibor Rak <rak@qbsw.sk>
2015-03-04 17:29:05 +0000

    CNUT-215

--- core/implementation/core-security/src/main/java/sk/qbsw/core/security/web/filter/CCorsAPIXSSAllowFilter.java
+++ core/implementation/core-security/src/main/java/sk/qbsw/core/security/web/filter/CCorsAPIXSSAllowFilter.java
@@ -1,43 +1,27 @@
 package sk.qbsw.core.security.web.filter;
 
-import java.io.IOException;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.web.filter.OncePerRequestFilter;
-
 /**
  * Filter for allowing call from other (all) domains. Setts following header attributes to response:
  * - Access-Control-Allow-Origin : *
  * - Access-Control-Allow-Methods : GET, POST, PUT, DELETE
  * - Access-Control-Allow-Headers : Content-Type
  * - Access-Control-Max-Age : 1800
- * 
- * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
- * 
+ *
  * @author Dalibor Rak
- * @version 1.12.0
+ * @version 1.13.0
  * @since 1.12.0
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
  */
-public class CCorsAPIXSSAllowFilter extends OncePerRequestFilter {
+public class CCorsAPIXSSAllowFilter extends ACustomCorsAPIXSSAllowFilter {
+
+	/** The Constant ALL. */
+	private static final String ALL = "*";
 
 	/* (non-Javadoc)
-	 * @see org.springframework.web.filter.OncePerRequestFilter#doFilterInternal(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, javax.servlet.FilterChain)
+	 * @see sk.qbsw.core.security.web.filter.ACustomCorsAPIXSSAllowFilter#getAllowedOrigin()
 	 */
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-		if (request.getHeader("Access-Control-Request-Method") != null && "OPTIONS".equals(request.getMethod())) {
-
-			// CORS "pre-flight" request
-			response.addHeader("Access-Control-Allow-Origin", "*");
-			response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-			response.addHeader("Access-Control-Allow-Headers", "Content-Type");
-			response.addHeader("Access-Control-Max-Age", "1800");//30 min
-		}
-		filterChain.doFilter(request, response);
+	protected String getAllowedOrigin() {
+		return ALL;
 	}
-
 }
