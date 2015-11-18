fd46302591554489bc33fb75ba52037bd1e3fe38
Tomáš Lauro <lauro@qbsw.sk>
2015-11-18 16:29:43 +0100

    MGAPP-259

--- core/implementation/core-security/src/main/java/sk/qbsw/core/security/service/spring/CSecurityAuthenticationProvider.java
+++ core/implementation/core-security/src/main/java/sk/qbsw/core/security/service/spring/CSecurityAuthenticationProvider.java
@@ -1,24 +1,13 @@
 package sk.qbsw.core.security.service.spring;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
-import sk.qbsw.core.base.exception.CSecurityException;
-import sk.qbsw.core.base.logging.annotation.CNotAuditLogged;
-import sk.qbsw.core.base.logging.annotation.CNotLogged;
-import sk.qbsw.core.base.service.AService;
 import sk.qbsw.core.security.model.domain.CUser;
 import sk.qbsw.core.security.model.spring.CUsernamePasswordUnitAuthentication;
-import sk.qbsw.core.security.service.IAuthenticationService;
 
 /**
  * Provider for spring security.
@@ -26,83 +15,37 @@ import sk.qbsw.core.security.service.IAuthenticationService;
  * @author Dalibor Rak
  * @author Tomas lauro
  * 
- * @version 1.11.5
+ * @version 1.13.4
  * @since 1.6.0
  * 
  */
 @Component ("customAuthenticationProvider")
-public class CSecurityAuthenticationProvider extends AService implements AuthenticationProvider
+public class CSecurityAuthenticationProvider extends ASecurityAuthenticationProvider
 {
-
-	/** The authentication service. */
-	@Autowired
-	private IAuthenticationService authenticationService;
-
-	/* (non-Javadoc)
-	 * @see org.springframework.security.authentication.AuthenticationProvider#authenticate(org.springframework.security.core.Authentication)
-	 */
-	@Override
-	public Authentication authenticate (@CNotLogged @CNotAuditLogged  Authentication authentication) throws AuthenticationException
-	{
-		Authentication retVal = null;
-		try
-		{
-			if (authentication instanceof CUsernamePasswordUnitAuthentication)
-			{
-				retVal = authenticate((CUsernamePasswordUnitAuthentication) authentication);
-			}
-		}
-		catch (CSecurityException e)
-		{
-			throw new AuthenticationServiceException("User not authenticated", e);
-		}
-
-		return retVal;
-	}
-
 	/* (non-Javadoc)
 	 * @see org.springframework.security.authentication.AuthenticationProvider#supports(java.lang.Class)
 	 */
 	@Override
 	public boolean supports (Class<? extends Object> authentication)
 	{
-		return authentication.equals(CUsernamePasswordUnitAuthentication.class);
+		return (CUsernamePasswordUnitAuthentication.class.isAssignableFrom(authentication.getClass()) == true);
 	}
 
-	/**
-	 * Authenticate user by login, password and unit.
-	 *
-	 * @param token the token
-	 * @return the authentication
-	 * @throws CSecurityException the c security exception
+	/* (non-Javadoc)
+	 * @see sk.qbsw.core.security.service.spring.ASecurityAuthenticationProvider#populateAuthentication(org.springframework.security.core.Authentication, sk.qbsw.core.security.model.domain.CUser, java.util.List)
 	 */
-	private Authentication authenticate (@CNotLogged @CNotAuditLogged CUsernamePasswordUnitAuthentication token) throws CSecurityException
+	@Override
+	protected Authentication populateAuthentication (Authentication authentication, CUser authenticatedUser, List<GrantedAuthority> authorities)
 	{
-		String login = token.getName();
-		String password = (String) token.getCredentials();
-		String unit = token.getUnit();
+		//get supported authentication
+		CUsernamePasswordUnitAuthentication usernameAuthentication = (CUsernamePasswordUnitAuthentication) authentication;
 
-		CUser user = null;
-		if (unit != null)
-		{
-			user = authenticationService.login(login, password, unit);
-		}
-		else
-		{
-			user = authenticationService.login(login, password);
-		}
+		//create fully populated authentication
+		CUsernamePasswordUnitAuthentication populatedAuthentication = new CUsernamePasswordUnitAuthentication(usernameAuthentication.getName(), usernameAuthentication.getCredentials(), authorities, usernameAuthentication.getUnit());
+		populatedAuthentication.setDetails(authenticatedUser);
 
-		List<String> roles = user.exportRoles();
-		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-		for (String role : roles)
-		{
-			authorities.add(new SimpleGrantedAuthority(role));
-		}
-
-		CUsernamePasswordUnitAuthentication retVal = new CUsernamePasswordUnitAuthentication(login, password, authorities, unit);
-		retVal.setDetails(user);
-
-		return retVal;
+		//return value
+		return populatedAuthentication;
 	}
 
 }
