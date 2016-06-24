67a49d1a9859d5fe8383d9112c33604665b7fd94
Tomáš Lauro <lauro@qbsw.sk>
2016-06-24 17:58:58 +0200

    Integracia FW BRW

--- core/implementation/core-base/src/main/java/sk/qbsw/core/base/dto/IDto.java
+++ core/implementation/core-base/src/main/java/sk/qbsw/core/base/dto/IDto.java
@@ -3,14 +3,7 @@ package sk.qbsw.core.base.dto;
 import java.io.Serializable;
 
 /**
- * The persistence dto interface.
- * 
- * @author Marian Oravec
- * @author Tomas Lauro
- * 
- * @version 1.15.0
- * @since 1.15.0
+ * The Interface IDTO.
  */
-public interface IDto extends Serializable
-{
+public interface IDTO extends Serializable {
 }
