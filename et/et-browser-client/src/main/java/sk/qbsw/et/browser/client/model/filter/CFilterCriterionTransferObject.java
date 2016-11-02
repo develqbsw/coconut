fc2b75ac24ed738163b83b0576e18e5ce435c7a0
Tomáš Lauro <lauro@qbsw.sk>
2016-11-02 17:29:20 +0100

    CNUT-269

--- et/et-browser-client/src/main/java/sk/qbsw/et/browser/client/model/filter/CFilterCriterionTransferObject.java
+++ et/et-browser-client/src/main/java/sk/qbsw/et/browser/client/model/filter/CFilterCriterionTransferObject.java
@@ -4,6 +4,9 @@
 package sk.qbsw.et.browser.client.model.filter;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.validation.constraints.NotNull;
 
@@ -30,11 +33,13 @@ public class CFilterCriterionTransferObject<F extends IFilterable> implements Se
 	@NotNull
 	private F property;
 
-	/** The string value. */
-	private String stringValue;
+	/** The value. */
+	@NotNull
+	private String value;
 
-	/** The number value. */
-	private Number numberValue;
+	/** The value type. */
+	@NotNull
+	private EValueType valueType;
 
 	/** The operator. */
 	@NotNull
@@ -51,16 +56,18 @@ public class CFilterCriterionTransferObject<F extends IFilterable> implements Se
 	/**
 	 * Instantiates a new c filter criterion.
 	 *
+	 * @param logicalOperator the logical operator
 	 * @param variable the variable
 	 * @param value the value
+	 * @param valueType the value type
 	 * @param operator the operator
 	 */
-	public CFilterCriterionTransferObject (ELogicalOperator logicalOperator, F variable, String stringValue, Number numberValue, EOperator operator)
+	public CFilterCriterionTransferObject (ELogicalOperator logicalOperator, F variable, String value, EValueType valueType, EOperator operator)
 	{
 		this.logicalOperator = logicalOperator;
 		this.property = variable;
-		this.stringValue = stringValue;
-		this.numberValue = numberValue;
+		this.value = value;
+		this.valueType = valueType;
 		this.operator = operator;
 	}
 
@@ -71,13 +78,18 @@ public class CFilterCriterionTransferObject<F extends IFilterable> implements Se
 	 */
 	public Serializable getValue ()
 	{
-		if (stringValue != null)
+		switch (valueType)
 		{
-			return stringValue;
-		}
-		else
-		{
-			return numberValue;
+			case STRING:
+				return value;
+			case NUMBER:
+				return Long.parseLong(value);
+			case DATE:
+				return LocalDate.parse(value, DateTimeFormatter.ISO_LOCAL_DATE);
+			case DATE_TIME:
+				return OffsetDateTime.parse(value, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+			default:
+				return value;
 		}
 	}
 
@@ -102,26 +114,6 @@ public class CFilterCriterionTransferObject<F extends IFilterable> implements Se
 	}
 
 	/**
-	 * Gets the string value.
-	 *
-	 * @return the string value
-	 */
-	public String getStringValue ()
-	{
-		return stringValue;
-	}
-
-	/**
-	 * Gets the number value.
-	 *
-	 * @return the number value
-	 */
-	public Number getNumberValue ()
-	{
-		return numberValue;
-	}
-
-	/**
 	 * Gets the operator.
 	 *
 	 * @return the operator
@@ -142,26 +134,6 @@ public class CFilterCriterionTransferObject<F extends IFilterable> implements Se
 	}
 
 	/**
-	 * Sets the string value.
-	 *
-	 * @param stringValue the new string value
-	 */
-	public void setStringValue (String stringValue)
-	{
-		this.stringValue = stringValue;
-	}
-
-	/**
-	 * Sets the number value.
-	 *
-	 * @param numberValue the new number value
-	 */
-	public void setNumberValue (Number numberValue)
-	{
-		this.numberValue = numberValue;
-	}
-
-	/**
 	 * Sets the operator.
 	 *
 	 * @param operator the new operator
@@ -169,5 +141,47 @@ public class CFilterCriterionTransferObject<F extends IFilterable> implements Se
 	public void setOperator (EOperator operator)
 	{
 		this.operator = operator;
+	}
+
+	/**
+	
+	/**
+	 * Sets the logical operator.
+	 *
+	 * @param logicalOperator the new logical operator
+	 */
+	public void setLogicalOperator (ELogicalOperator logicalOperator)
+	{
+		this.logicalOperator = logicalOperator;
+	}
+
+	/**
+	 * Gets the value type.
+	 *
+	 * @return the value type
+	 */
+	public EValueType getValueType ()
+	{
+		return valueType;
+	}
+
+	/**
+	 * Sets the value type.
+	 *
+	 * @param valueType the new value type
+	 */
+	public void setValueType (EValueType valueType)
+	{
+		this.valueType = valueType;
+	}
+
+	/**
+	 * Sets the value.
+	 *
+	 * @param value the new value
+	 */
+	public void setValue (String value)
+	{
+		this.value = value;
 	}
 }
---
+++ et/et-browser-client/src/main/java/sk/qbsw/et/browser/client/model/filter/EValueType.java
@@ -0,0 +1,23 @@
+package sk.qbsw.et.browser.client.model.filter;
+
+/**
+ * The value type.
+ *
+ * @author Tomas Lauro
+ * @version 1.16.0
+ * @since 1.16.0
+ */
+public enum EValueType
+{
+	/** The string. */
+	STRING,
+
+	/** The number. */
+	NUMBER,
+
+	/** The date. */
+	DATE,
+
+	/** The date time. */
+	DATE_TIME;
+}
