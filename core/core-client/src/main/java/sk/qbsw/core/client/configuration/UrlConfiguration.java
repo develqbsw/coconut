9418d4ba6b4854a7f090e6286665b56ecda04b99
Tomáš Lauro <lauro@qbsw.sk>
2017-06-27 16:40:15 +0200

    CNUT-286

---
+++ core/core-client/src/main/java/sk/qbsw/core/client/configuration/UrlConfiguration.java
@@ -0,0 +1,25 @@
+package sk.qbsw.core.client.configuration;
+
+/**
+ * The URL configuration.
+ * 
+ * @author Tomas Lauro
+ * @version 1.18.0
+ * @since 1.18.0
+ */
+public interface UrlConfiguration
+{
+	String getScheme ();
+
+	String getHostName ();
+
+	Integer getPort ();
+
+	String getPath ();
+
+	String getProxyHostName ();
+
+	Integer getProxyPort ();
+
+	boolean isUntrustedSslConnectionAllowed ();
+}
